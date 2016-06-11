8240bc1d2f1304b43334e63801ce713f71a491e2
Eric Chen <ericchen0121@Erics-MacBook-Pro.local>
2016-06-11 16:50:15 -0700

    remove fluff and validate tests

--- test/pluginStdDevTest.php
+++ test/pluginStdDevTest.php
@@ -16,7 +16,7 @@ class PluginStdDevTest extends PHPUnit_Framework_TestCase {
     return $dict;
   }
 
-  public function testAverageIsCorrect() {
+  public function testStdDevIsCorrect() {
     $day = $this->genRange(0, 96);
     $week = $this->genRange(0, 28);
     $week4 = $this->genRange (0, 112);
@@ -26,22 +26,30 @@ class PluginStdDevTest extends PHPUnit_Framework_TestCase {
       'week4' => $week4
     );
     echo $params;
-    $result = plugin_average($params);
+    $result = plugin_standarddeviation($params);
     $this->assertCount(3, $result);
     $this->assertCount(48, $result['day']);
     $this->assertCount(14, $result['week']);
     $this->assertCount(56, $result['week4']);
     $this->assertEquals(
         "14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14",
-        implode(',', $result['day'],'', 0.0001)
+        implode(', ', $result['day']),'', 0.0001
     );
+    foreach ($result['week'] as $key => $value) {
+      $result['week'][$key] = round($result['week'][$key], 4); 
+    }
+    
     $this->assertEquals(
         "4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833, 4.1833",
-        implode(',', $result['week'],'', 0.0001)
+        implode(', ', $result['week']),'', 0.0001
     );
+
+    foreach ($result['week4'] as $key => $value) {
+      $result['week4'][$key] = round($result['week4'][$key], 4); 
+    }
     $this->assertEquals(
-        "16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309, 16.309",
-        implode(',', $result['week4'],'', 0.0001)
+        "16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095, 16.3095",
+        implode(', ', $result['week4']),'', 0.0001
     );
   }
 }
